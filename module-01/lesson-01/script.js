df7350087d1d130c9c36908b136cf2dd59386f48
Ivon Lagta <islaguta@gmail.com>
2026-01-09 15:20:07 +0100

    update commit message

---
+++ module-01/lesson-01/script.js
@@ -0,0 +1,37 @@
+// 10
+const elementOut10 = document.querySelector(".out-10");
+console.log("🚀 ~ elementOut10:", elementOut10);
+elementOut10.innerHTML = "<h2>Hi</h2>";
+
+// 11
+const elementOut11 = document.querySelector(".out-11");
+console.log("🚀 ~ elementOut11:", elementOut11);
+elementOut11.innerHTML = "123";
+elementOut11.innerHTML = "456";
+
+// elementOut11.innerHTML = elementOut11.innerHTML + "456";
+
+// 12
+const a = document.querySelector(".out-12");
+console.log("🚀 ~ a:", a);
+a.innerHTML = 3.1415;
+// a.innerHTML = Math.ceil(3.1415);
+
+// 13
+const out13 = document.querySelector(".out-13");
+console.log("🚀 ~ out13:", out13);
+out13.innerHTML =
+  '<img src="https://images.unsplash.com/photo-1579468118864-1b9ea3c0db4a" alt="js" width="400" >';
+
+// 14
+const z1 = 6;
+const z2 = 3;
+const out14 = document.querySelector(".out-14");
+out14.innerHTML = z1 * z2;
+
+// 19
+let out19 = document.querySelector(".out19");
+console.log("🚀 ~ out19:", out19);
+out19 = document.querySelector(".out-19-test");
+
+// 20
---
+++ module-01/lesson-02/script.js
@@ -0,0 +1,26 @@
+// 1
+
+const a = 7;
+const b = 9;
+
+const result = a * b;
+console.log("🚀 ~ result:", result);
+
+// 2
+const c = 7;
+const d = 9;
+
+const out2 = document.querySelector(".out-2");
+console.log("🚀 ~ out2:", out2);
+
+out2.textContent = c / d;
+
+// 3
+const e1 = 3;
+const f1 = 5;
+
+const out3 = document.querySelector(".out-3");
+console.log("🚀 ~ out3:", out3);
+out3.textContent = e1 + f1;
+
+// 4
